4060092a96068d4f4593ec2ad4c698034dfb98ba
Christopher Kolstad <chriswk@getunleash.ai>
2021-04-13 23:54:48 +0200

    Setup postgres env as well

--- knexfile.ts
+++ knexfile.ts
@@ -1,22 +1,14 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: "postgresql",
-    connection: {
-      database:'postgres',
-      user: 'postgres',
-      password: 'postgres'
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   local: {
     client: "postgresql",
     connection: "postgresql://knex:password@localhost:5432/session"
+  },
+
+  postgres: {
+    client: "postgresql",
+    connection: "postgresql://postgresql:postgresql@localhost:5432/postgres"
   },
 
   sqlite3: {
